29096abe5d023d5eac2f9e3c3b35beb746fcba89
Phalgun Vaddepalli <phalgunv@users.noreply.github.com>
2023-04-28 20:35:58 +0000

    added jest-html-reporters

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   testEnvironment: 'node',
-	"reporters": [ "default", "jest-junit", ["./node_modules/jest-html-reporter", {
-		"pageTitle": "Test Report"
-	}]
+	"reporters": [ "default", 
+	["jest-junit", {
+		outputDirectory: 'reports',
+		outputName: 'jest-junit.xml',
+	}], 
+	["./node_modules/jest-html-reporter", {
+		"pageTitle": "jest-html-reporter",
+		"outputPath": "reports/jest-html-reporter.html"
+	}],
+	['jest-html-reporters', { "publicPath": "reports" }]
   ]
 };
